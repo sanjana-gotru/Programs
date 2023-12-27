1ac8c6236c07717e456455b23fc6ec85907ce4fe
sanjana-gotru <155001106+sanjana-gotru@users.noreply.github.com>
2023-12-27 22:30:32 +0530

    Create gcdwithrecursion.c

---
+++ gcdwithrecursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int gcd (int n1,int n2);
+int main()
+{
+int n1,n2;
+printf ("enter two positive integers");
+scanf ("%d%d",&n1,&n2);
+printf ("gcd of %d and %d is %d",n1,n2,gcd(n1,n2));
+return 0;
+}
+int gcd (int x,int y)
+{
+if (y!=0)
+return gcd(y,x%y);
+else 
+return x;
+}
